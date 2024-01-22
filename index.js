0100249621dcfa228ab6907bc4aae5887a7e0eda
sollydz <157352727+sollydz@users.noreply.github.com>
2024-01-22 00:05:35 -0800

    index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ keepAlive();
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    timeZone: 'Asia/Ho_Chi_Minh', //https://www.zeitverschiebung.net/en/ and find your city and enter here
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
@@ -23,26 +23,26 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('1170948236769493052')
+    .setApplicationId('1116621382466412614')
     .setType('STREAMING')
     .setURL('https://twitch.tv/developer') //Must be a youtube video link 
-    .setState('お願い！ﾀﾋんでくれ')
-    .setName('maso')
-    .setDetails(`།𓎟𓎟𓎟†𓎟𓎟𓎟།`)
+    .setState('我讨厌数学')
+    .setName('soly')
+    .setDetails(`━╬٨ـﮩﮩ❤٨ـﮩﮩـ╬━`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1011444419838353429/1198449882822885467/33C636B2-9B0C-4B41-BB68-8C4DF0F6B7A9_8.gif?ex=65bef26e&is=65ac7d6e&hm=1c56b2eaee6dc1e987267f95e5de06549857c58c89e5372e12fbe059d64804f9&') //You can put links in tenor or discord and etc.
-.setAssetsSmallImage('https://cdn.discordapp.com/attachments/1011444419838353429/1198449882428625017/IMG_5774.gif?ex=65bef26e&is=65ac7d6e&hm=5f2ab6e0ea470cc7beb597f5363717c581c8ef43279d832356349f9e31e890b7&')
-    .setAssetsLargeText('なりたい　なれない') //Text when you hover the Large image
-    .addButton('♡', 'https://rentry.co/')
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1116694463658598412/1198894308275781742/9cKIHbWbbX3e4qM4itasnocarteanime.gif?') //You can put links in tenor or discord and etc.
+.setAssetsSmallImage('small_img')
+    .setAssetsLargeText('几何导致缓慢而痛苦的死亡') //Text when you hover the Large image
+    .addButton('⛓', 'https://discord.com/invite/UsYZRFFfrK/')
 
   client.user.setActivity(r);
-  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
+  client.user.setPresence({ status: "online" }); //dnd, online, idle, offline
 
   let prevTime = null;
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `།𓎟𓎟𓎟†𓎟𓎟𓎟།`;
+      const newDetails = `━╬٨ـﮩﮩ❤٨ـﮩﮩـ╬━`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
